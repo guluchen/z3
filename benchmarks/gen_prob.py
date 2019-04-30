c69df8c1a4cd0ae118f283d0b0503933f3a0b20a
Hsin-Hung Lin <linil2j@gmail.com>
2019-04-30 17:53:05 +0800

    Add length constraint generation in script `gen_prob.py`

--- benchmarks/gen_prob.py
+++ benchmarks/gen_prob.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
-
 import os
 import random
 
 from argparse import ArgumentParser
 from enum import Enum, unique, auto
-from typing import List, Set
+from typing import List, Set, Dict
 
 WORD_SIZE_MAX = int(os.getenv('WORD_SIZE_MAX', 6))
 WORD_SIZE_MIN = int(os.getenv('WORD_SIZE_MIN', 1))
 CONSTRAINT_NUMBER = int(os.getenv('CONSTRAINT_NUMBER', 3))
+COEFFICIENT_RANGE = int(os.getenv('COEFFICIENT_RANGE', 2))
 SYMBOLS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 
 
@@ -118,6 +118,53 @@ class WordConstraint:
         return f'(not {equation})' if self.should_fail else equation
 
 
+class LengthConstraint:
+    def __init__(self, elems: Set[Element] = set()):
+        self.coefficients: Dict[Element, int] = dict()
+        self.constant = 0
+        if len(elems) > 0:
+            self.set_coefficients(elems)
+
+    def __str__(self):
+        return '+'.join([f'({self.coefficients[e]})({e.value})' for e in self.coefficients.keys()]) + \
+               f'({self.constant})>=0'
+
+    def set_coefficients(self, elems: Set[Element]):
+        assert len([1 for e in elems if not is_var(e)]) == 0
+        while len(self.coefficients) == 0:  # make sure at least one non-zero coefficient
+            for e in elems:
+                coeff = random.choice([-COEFFICIENT_RANGE, COEFFICIENT_RANGE+1])
+                if coeff != 0:  # only set non-zero cofficient
+                    self.coefficients[e] = coeff
+                if len(self.coefficients) == CONSTRAINT_NUMBER:  # take most coefficients as number of word constraints
+                    break
+        self.constant = random.choice([-COEFFICIENT_RANGE, COEFFICIENT_RANGE])
+        if self.constant >= 0:
+            self.constant += 1  # constant always non-zero
+
+    def render(self) -> str:
+        if not self.coefficients:
+            return ''
+        ret = ''
+        for e in self.coefficients.keys():
+            if self.coefficients[e] < 0:  # minus
+                str_coeff = f'(- {-self.coefficients[e]})'
+            else:
+                str_coeff = f'{self.coefficients[e]}'
+            if self.coefficients[e] == 1:
+                str_tmp = f'(str.len {e.value})'
+            else:
+                str_tmp = f'(* {str_coeff} (str.len {e.value}))'
+            ret += f' {str_tmp}'
+        # constant
+        if self.constant < 0:  # minus
+            str_const = f'(- {-self.constant})'
+        else:
+            str_const = f'{self.constant}'
+        ret = f'(assert (>= (+{ret} {str_const}) 0))'
+        return ret
+
+
 class Problem:
     @classmethod
     def random(cls):
@@ -143,6 +190,7 @@ class Problem:
             self.word_constraints.append(WordConstraint())
         self.word_constraints[0].should_fail = False  # at least one equation
         self.word_constraints[-1].should_fail = True  # at least one inequation
+        self.length_constraint = LengthConstraint()
 
     def __repr__(self):
         return '.'.join(map(repr, self.word_constraints))
@@ -179,6 +227,10 @@ class Problem:
         for w in self.word_constraints:
             w.should_fail = False
 
+    def add_length_constraint(self):
+        self.length_constraint.set_coefficients(self.variables())
+        # print(f'added length constraint:\n    {self.length_constraint.render()}\n')
+
     def render(self) -> str:
         assert self.is_full()
 
@@ -187,7 +239,9 @@ class Problem:
             result += f'(declare-fun {v.value} () String)\n'
         for w in self.word_constraints:
             result += f'(assert {w.render()})\n'
-        result += '(check-sat)\n(get-model)\n'
+        result += f'{self.length_constraint.render()}\n'
+        # result += '(check-sat)\n(get-model)\n'
+        result += '(check-sat)\n'
         return result
 
 
@@ -207,17 +261,21 @@ def main():
     arg_parser.add_argument('--disequality',
                             help='generate problems having disequalities (default: off)',
                             dest='disequality', action='store_true')
+    arg_parser.add_argument('--lenc',
+                            help='generate problems with length polynomial length constraint (default: off)',
+                            dest='lenc', action='store_true')
     args = arg_parser.parse_args()
 
     prob_num = int(args.NUM)
     output_dir = args.d
     file_prefix = args.p
     print('Generating problems...')
-    print(f'  file prefix: {file_prefix}')
-    print(f'   output dir: {output_dir}')
-    print(f'  problem num: {prob_num}')
-    print(f'non-quadratic: {args.non_quadratic}')
-    print(f'  disequality: {args.disequality}')
+    print(f'      file prefix: {file_prefix}')
+    print(f'       output dir: {output_dir}')
+    print(f'      problem num: {prob_num}')
+    print(f'    non-quadratic: {args.non_quadratic}')
+    print(f'      disequality: {args.disequality}')
+    print(f'length constraint: {args.lenc}')
 
     # Proceed problem generation
     problem_set: Set[Problem] = set()
@@ -231,6 +289,9 @@ def main():
     if not args.disequality:
         for p in problem_set:
             p.equalize_all_constraints()
+    if args.lenc:
+        for p in problem_set:
+            p.add_length_constraint()
 
     # Output the generated problems
     os.makedirs(output_dir) if not os.path.exists(output_dir) else None
